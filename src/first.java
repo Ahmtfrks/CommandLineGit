0f7089fde1a9ed1f814005591381932b6cb4604d
Ahmtfrks <140855330+Ahmtfrks@users.noreply.github.com>
2023-11-01 19:56:59 +0100

    Update first.java

--- src/first.java
+++ src/first.java
@@ -2,5 +2,11 @@ public class first {
     public static void main(String[] args) {
 
         System.out.println("main");
+
+        //adding more new things
+
+        //more things
+
+        //made from origin
     }
 }
